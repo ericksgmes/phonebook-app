b9b2624f956f97ababcb43a1a8a081df75200cc8
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-06-10 21:37:40 -0300

    chore: create ConsultContactPage file.

---
+++ src/test/java/phonebook/test/pages/ConsultContactPage.java
@@ -0,0 +1,14 @@
+package phonebook.test.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ConsultContactPage {
+    private WebDriver driver;
+
+    public ConsultContactPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
