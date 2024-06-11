fb6169395a1ea4015d39190d0b6b2c82df50451b
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-06-10 21:58:14 -0300

    feat: add wait for elem load.

--- src/test/java/phonebook/test/DeleteContactTests.java
+++ src/test/java/phonebook/test/DeleteContactTests.java
@@ -3,6 +3,7 @@ package phonebook.test;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.By;
 import phonebook.test.pages.AddContactPage;
 import phonebook.test.pages.ConsultContactPage;
 import phonebook.test.pages.LandingPage;
@@ -25,13 +26,19 @@ public class DeleteContactTests extends BaseTest {
     @DisplayName("Should exclude valid contact")
     public void shouldExcludeValidContact() {
         landingPage.open();
+        waitForElementToLoad(driver, By.linkText("Adicionar"));
         landingPage.clickAddButton();
+        waitForElementToLoad(driver, By.id("nome"));
         addContactPage.enterName(faker.name().fullName());
+        waitForElementToLoad(driver, By.id("tel"));
         addContactPage.enterPhone(faker.phoneNumber().cellPhone());
+        waitForElementToLoad(driver, By.cssSelector(".button-create"));
         addContactPage.clickCreate();
         acceptAlert("Contato adicionado com sucesso!");
         landingPage.open();
+        waitForElementToLoad(driver, By.linkText("Consultar"));
         landingPage.clickConsultButton();
+        waitForElementToLoad(driver, By.cssSelector("tr:nth-child(1) .button-read:nth-child(2)"));
         consultContactPage.clickFirstDeleteButton();
         acceptAlert("Tem certeza de que deseja excluir este contato?");
         acceptAlert("Contato excluído com sucesso!");
