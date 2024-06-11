a7e85f601467e7dd72da8734f7991fac2c40e820
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-06-10 21:12:33 -0300

    feat: create open method.

--- src/test/java/phonebook/test/pages/LandingPage.java
+++ src/test/java/phonebook/test/pages/LandingPage.java
@@ -1,4 +1,14 @@
 package phonebook.test.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class LandingPage {
+    private WebDriver driver;
+    public LandingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public void open() {
+        driver.get("http://localhost/phonebook/");
+        driver.manage().window().maximize();
+    }
 }
