ab6d03cba899ec68b634444e60b640c31db9562e
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-06-10 23:12:54 -0300

    refactor: add button wait until clicable.

--- src/test/java/phonebook/test/pages/ConsultContactPage.java
+++ src/test/java/phonebook/test/pages/ConsultContactPage.java
@@ -3,6 +3,10 @@ package phonebook.test.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ConsultContactPage {
     private WebDriver driver;
@@ -12,11 +16,15 @@ public class ConsultContactPage {
     }
 
     public void clickFirstReadButton() {
-        driver.findElement(By.cssSelector("tr:nth-child(1) .button-read:nth-child(1)")).click();
+        final WebElement button = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("tr:nth-child(1) .button-read:nth-child(1)")));
+        button.click();
     }
 
     public void clickFirstDeleteButton() {
-        driver.findElement(By.cssSelector("tr:nth-child(1) .button-read:nth-child(2)")).click();
+        final WebElement button = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("tr:nth-child(1) .button-read:nth-child(2)")));
+        button.click();
     }
 
     public WebElement getFirstContactPhoneInput() {
