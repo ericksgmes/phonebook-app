3257276412339e694d0d034e709a9e8e123dbcf2
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-06-10 21:05:49 -0300

    feat: create setUp method.

--- src/test/java/phonebook/test/BaseTest.java
+++ src/test/java/phonebook/test/BaseTest.java
@@ -1,4 +1,19 @@
 package phonebook.test;
 
+import com.github.javafaker.Faker;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected Faker faker;
+
+    @Before
+    public void setUp() {
+        driver = DriverSetup.getDriver();
+        faker = new Faker();
+    }
 }
