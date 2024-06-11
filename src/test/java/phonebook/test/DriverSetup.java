412c23ca6843e159753a28c511c9ab298fa4418a
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-06-10 21:04:32 -0300

    feat: create getDriver method.

--- src/test/java/phonebook/test/DriverSetup.java
+++ src/test/java/phonebook/test/DriverSetup.java
@@ -1,4 +1,21 @@
 package phonebook.test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class DriverSetup {
+    public static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            return new ChromeDriver(options);
+        }
+        return driver;
+    }
 }
